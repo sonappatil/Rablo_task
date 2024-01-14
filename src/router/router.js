b6eb2faf8c8d215e0f7de83100177cfa38b47bec
Sonali <patilsonalias002@gmail.com>
2024-01-14 21:03:09 +0530

    otp via email

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const twilio = require("twilio");
+const nodemailer = require("nodemailer");
 const User = require("../models/personal-details");
 const OTP = require("../models/otpModel");
+
 
 const router = express.Router();
 
@@ -11,29 +13,41 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = twilio(accountSid, authToken);
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "rocio.weissnat86@ethereal.email",
+    pass: "PeD6M87NvzBXs9ybPA",
+  }
+});
+
+
 function generateOTP() {
-        const otp = Math.floor(1000 + Math.random() * 9000);
-        return otp.toString();
-      }
+  const otp = Math.floor(1000 + Math.random() * 9000);
+  return otp.toString();
+}
 
 router.post("/createUser", async (req, res) => {
   try {
 
-    const { firstName, dateOfBirth, gender, mobileNumber, houseNo, city, state, pinCode, referralCode, schoolName, className, board, subjects } = req.body;
+    const { firstName, email, dateOfBirth, gender, mobileNumber, houseNo, city, state, pinCode, referralCode, schoolName, className, board, subjects } = req.body;
     //console.log(req.body);
 
-    const existingUser = await User.findOne({ mobileNumber });
+    const existingUser = await User.findOne({ $or: [{ mobileNumber }, { email }] });
     if (existingUser) {
       return res.status(400).json({ message: "User is already registered" });
     }
 
-    if (!firstName || !dateOfBirth || !gender || !mobileNumber || !houseNo || !city || !state || !pinCode || !schoolName || !className || !board || !subjects) {
+    if (!firstName || !email || !dateOfBirth || !gender || !mobileNumber || !houseNo || !city || !state || !pinCode || !schoolName || !className || !board || !subjects) {
       return res.status(400).json({ message: "all the fields are required" });
     }
 
 
     const user = new User({
-      firstName, dateOfBirth, gender, mobileNumber, houseNo, city, state, pinCode, referralCode, schoolName, className, board, subjects
+      firstName, email, dateOfBirth, gender, mobileNumber, houseNo, city, state, pinCode, referralCode, schoolName, className, board, subjects
     });
 
     await user.save();
@@ -45,74 +59,147 @@ router.post("/createUser", async (req, res) => {
 });
 
 
-router.post("/send-otp", async(req,res)=>{
+router.post("/send-otp", async (req, res) => {
   try {
     const { mobileNumber } = req.body;
 
-    const user = await User.findOne({ mobileNumber});
+    const user = await User.findOne({ mobileNumber });
 
     if (!user) {
-      return res.status(404).json({ message: "User is not found"});
+      return res.status(404).json({ message: "User is not found" });
     }
 
     const otpValue = generateOTP();
 
     const otpRecord = new OTP({
       user: user._id,
-      otp : otpValue,
+      otp: otpValue,
     });
 
     await otpRecord.save();
 
     await twilioClient.messages.create({
       body: `Your OTP for verification is: ${otpValue}`,
-      to : `+91${mobileNumber}`,
-      from : twilioPhoneNumber,
+      to: `+91${mobileNumber}`,
+      from: twilioPhoneNumber,
     });
 
-    res.status(200).json({message : "OTP sent successfully"});
+    res.status(200).json({ message: "OTP sent successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-router.post("/verify-otp",  async(req,res)=>{
+router.post("/verify-otp", async (req, res) => {
   try {
-    const { mobileNumber, otp} = req.body;
+    const { mobileNumber, otp } = req.body;
 
-    const user = await User.findOne({ mobileNumber});
+    const user = await User.findOne({ mobileNumber });
 
-    if(!user){
-      return res.status(404).json({ message : "User not Found"});
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
     }
 
-    const otpRecord =  await OTP.findOne({ user : user._id, otp });
+    const otpRecord = await OTP.findOne({ user: user._id, otp }).sort({ _id: -1 });
 
-    if(!otpRecord){
-      return res.status(400).json({ message : "Invalid OTP"});
+    if (!otpRecord) {
+      return res.status(400).json({ message: "Invalid OTP" });
     }
-   
+
     if (otpRecord.otp !== otp) {
       return res.status(400).json({ message: "Mismatched OTP" });
-    } 
+    }
 
     if (otpRecord.timer < 0) {
-      return res.status(400).json({ message : "OTP expired"});
+      return res.status(400).json({ message: "OTP expired" });
     }
 
     user.status = "Verified";
-   await user.save();
+    await user.save();
 
-  await OTP.findByIdAndDelete(otpRecord._id);
+    await OTP.findByIdAndDelete(otpRecord._id);
 
-    res.status(200).json({user, message :  "OTP verified successfully"});
+    res.status(200).json({ user, message: "OTP verified successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 })
 
+router.post("/email/send-otp", async (req, res) => {
+  try {
+    const { email } = req.body;
 
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User is not found" });
+    }
+
+    const otpValue = generateOTP();
+
+    const otpRecord = new OTP({
+      user: user._id,
+      otp: otpValue,
+    });
+
+    await otpRecord.save();
+
+    const mailOptions = {
+      from: "<rocio.weissnat86@ethereal.email>",
+      to: email,
+      subject: "OTP Verification",
+      text: `Your OTP for verification is: ${otpValue}`,
+    };
+
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send({ message: 'Error sending email' });
+      } else {
+        console.log('Email sent:', info.response);
+        res.status(201).send({ message: "OTP sent successfully", messageId: info.messageId  });
+      }
+    });
+
+    //res.status(200).json({ message: "OTP sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+})
+
+router.post("/email/verify-otp", async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+
+    const otpRecord = await OTP.findOne({ user: user._id, otp }).sort({ _id: -1 });
+
+    if (!otpRecord) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    if (otpRecord.timer < 0) {
+      return res.status(400).json({ message: "OTP expired" });
+    }
+
+    user.status = "Verified";
+    await user.save();
+
+    await OTP.findByIdAndDelete(otpRecord._id);
+
+    res.status(200).json({ user, message: "OTP verified successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+})
 
 module.exports = router;
